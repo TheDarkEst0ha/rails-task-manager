e090d9ccb34997134fe08a110c3a419947ab3dab
rafaela yazawa <yazawrafa@gmail.com>
2025-02-11 23:29:13 +0900

    task manager completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # new -> only displays the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  get '/task/:id', to: 'tasks#show', as: :task
+
+  #  create -> save
+  post '/tasks', to: 'tasks#create'
+
+  # edit -> just displays the form
+  # I need the id to edit because I want to edit only one task
+  get '/task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/task/:id', to: 'tasks#update'
+
+  delete '/task/:id', to: 'tasks#destroy'
 end
